beee0dd62bd2a7e75faa141bfd600e867872b78d
Cory Rylan <crylan@nvidia.com>
2026-05-14 19:26:11 -0500

    chore(ci): lighthouse
    Signed-off-by: Cory Rylan <crylan@nvidia.com>

--- projects/code/src/codeblock/codeblock.test.lighthouse.ts
+++ projects/code/src/codeblock/codeblock.test.lighthouse.ts
@@ -18,6 +18,6 @@ describe('codeblock lighthouse report', () => {
     expect(report.scores.performance).toBe(100);
     expect(report.scores.accessibility).toBe(100);
     expect(report.scores.bestPractices).toBe(100);
-    expect(report.payload.javascript.kb).toBeLessThan(19.6);
+    expect(report.payload.javascript.kb).toBeLessThan(19.7);
   });
 });
